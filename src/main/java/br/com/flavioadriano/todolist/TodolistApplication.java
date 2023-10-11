578be639d9579055fdab2d3f18d704331cedbb1b
FravonDev <flvsadr@gmail.com>
2023-10-10 21:49:26 -0300

    feat: getter & setters, controller

--- src/main/java/br/com/flavioadriano/todolist/TodolistApplication.java
+++ src/main/java/br/com/flavioadriano/todolist/TodolistApplication.java
@@ -2,6 +2,7 @@ package br.com.flavioadriano.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//perceba que a cor do pacote é diferente da cor da Classe
 
 @SpringBootApplication
 public class TodolistApplication {
